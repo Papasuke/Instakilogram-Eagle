174e088e349ce33f0cb01d5a6ee36b258bbb989a
Papasuke <94671577+Papasuke@users.noreply.github.com>
2022-05-08 06:43:34 +0700

    Add files via upload

---
+++ Instakilogram/register/script.js
@@ -0,0 +1,9 @@
+var form = document.getElementById("inside-form");
+
+function openRegister(){
+    form.style.transform = "rotateY(-180deg)"
+}
+
+function openLogin(){
+    form.style.transform = "rotateY(0deg)"
+}
