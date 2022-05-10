6be08a0e4f515b0318b7a3b4f0a4a5b890d6ca58
Papasuke <94671577+Papasuke@users.noreply.github.com>
2022-05-10 14:39:29 +0700

    Delete Instakilogram/homepage-logined directory

--- Instakilogram/homepage-logined/script.js
+++
@@ -1,8 +0,0 @@
-var menuSections = document.querySelector(".menu-icon");
-var sidebar = document.querySelector(".left-sidebar");
-var middleSide = document.querySelector(".middle-side");
-
-menuSections.onclick = function(){
-    sidebar.classList.toggle("shortcut-sidebar")
-    middleSide.classList.toggle("full-middle-side")
-}
